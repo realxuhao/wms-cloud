be1c917a1c7948b5c6c0b97fe28ed9b9dceb633f
gzj <2424834965@qq.com>
2022-09-27 16:15:30 +0800

    wms

---
+++ wms-gateway/src/main/java/com/bosch/gateway/WMSGatewayApplication.java
@@ -0,0 +1,29 @@
+package com.bosch.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+/**
+ * 网关启动程序
+ * 
+ * @author ruoyi
+ */
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class WMSGatewayApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(WMSGatewayApplication.class, args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  wms网关启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
+                " .-------.       ____     __        \n" +
+                " |  _ _   \\      \\   \\   /  /    \n" +
+                " | ( ' )  |       \\  _. /  '       \n" +
+                " |(_ o _) /        _( )_ .'         \n" +
+                " | (_,_).' __  ___(_ o _)'          \n" +
+                " |  |\\ \\  |  ||   |(_,_)'         \n" +
+                " |  | \\ `'   /|   `-'  /           \n" +
+                " |  |  \\    /  \\      /           \n" +
+                " ''-'   `'-'    `-..-'              ");
+    }
+}
