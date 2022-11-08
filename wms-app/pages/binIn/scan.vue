d2fdf21f07963b425e2a96bf71bf54c2d23cd8fe
huangcheng <cheng.huang2@cn.bosch.com>
2022-11-08 11:03:31 +0800

    扫描页跳转

--- wms-app/pages/binIn/scan.vue
+++ wms-app/pages/binIn/scan.vue
@@ -1,6 +1,6 @@
 <template>
 	<my-page nav-title="扫描SSCC码" >
-			<view class="content" slot="page-main" @click="handleGotoCount">
+			<view class="content" slot="page-main" @click="handleGotoOperation">
 				<image src="/static/sku-phone.png" class="m-b-8"></image>
 				<text>请将激光扫描头对准SSCC码区域</text>
 			</view>
