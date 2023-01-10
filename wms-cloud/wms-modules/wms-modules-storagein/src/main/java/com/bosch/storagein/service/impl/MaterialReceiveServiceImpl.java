39773386b35d1663e581227fd431ab66a0ced417
xuhao <xuhao2021@gusulab.ac.cn>
2023-01-10 13:48:03 +0800

    fix:收货清单上传bug

--- wms-cloud/wms-modules/wms-modules-storagein/src/main/java/com/bosch/storagein/service/impl/MaterialReceiveServiceImpl.java
+++ wms-cloud/wms-modules/wms-modules-storagein/src/main/java/com/bosch/storagein/service/impl/MaterialReceiveServiceImpl.java
@@ -86,6 +86,7 @@ public class MaterialReceiveServiceImpl extends ServiceImpl<MaterialRecevieMappe
 
         LambdaQueryWrapper<MaterialReceive> lambdaQueryWrapper = new LambdaQueryWrapper<>();
         lambdaQueryWrapper.in(MaterialReceive::getSsccNumber, codes);
+        lambdaQueryWrapper.eq(MaterialReceive::getDeleteFlag, DeleteFlagStatus.FALSE.getCode());
         Integer res = materialRecevieMapper.selectCount(lambdaQueryWrapper);
         return res > 0;
     }
