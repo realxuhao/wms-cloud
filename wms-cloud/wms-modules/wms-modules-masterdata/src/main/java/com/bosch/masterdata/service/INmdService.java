c33d95fc25fdb026871da57971488f271a01c2e7
gzj <2424834965@qq.com>
2023-02-09 15:44:43 +0800

    nmd导入

--- wms-cloud/wms-modules/wms-modules-masterdata/src/main/java/com/bosch/masterdata/service/INmdService.java
+++ wms-cloud/wms-modules/wms-modules-masterdata/src/main/java/com/bosch/masterdata/service/INmdService.java
@@ -2,6 +2,7 @@ package com.bosch.masterdata.service;
 
 import com.bosch.masterdata.api.domain.Nmd;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.bosch.masterdata.api.domain.dto.MaterialDTO;
 import com.bosch.masterdata.api.domain.dto.NmdDTO;
 import com.bosch.masterdata.api.domain.vo.NmdVO;
 
@@ -14,5 +15,46 @@ import java.util.List;
 */
 public interface INmdService extends IService<Nmd> {
 
+    /**
+     * 查询nmdlist
+     * @param nmdDTO
+     * @return
+     */
     List<NmdVO> selectList(NmdDTO nmdDTO);
+
+    /**
+     * 查询单个nmd 信息
+     * @param id
+     * @return
+     */
+    NmdVO selectNmdById(Long id);
+
+    /**
+     * 插入nmd
+     * @param nmdDTO
+     * @return
+     */
+    Integer insertNmd(NmdDTO nmdDTO);
+
+    /**
+     * 更新nmd
+     * @param nmdDTO
+     * @return
+     */
+    Integer updateNmd(NmdDTO nmdDTO);
+
+    /**
+     * 删除nmd
+     * @param ids
+     * @return
+     */
+    Integer deleteNmd(Long[] ids);
+
+    /**
+     * 查询信息是否重复
+     *
+     * @param nmdDTOS
+     * @return
+     */
+    boolean validNmdList(List<NmdDTO> nmdDTOS);
 }
