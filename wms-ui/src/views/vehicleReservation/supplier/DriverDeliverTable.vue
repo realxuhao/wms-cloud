ac3e727d9f89168df36a533c5f92e9cfac2a247a
APAC\bsu1szh <sherry.yu@bosch.com>
2023-02-16 17:14:02 +0800

    add vf-Driver_Deliver table

---
+++ wms-ui/src/views/vehicleReservation/supplier/DriverDeliverTable.vue
@@ -0,0 +1,161 @@
+<template>
+  <div class="wrapper">
+    <!-- table -->
+    <div class="table-content">
+      <a-form layout="inline" class="search-content">
+        <a-row :gutter="16">
+          <a-col :span="4">
+            <a-form-model-item label="司机姓名">
+              <a-input v-model="queryForm.driverName" placeholder="司机姓名" allow-clear/>
+            </a-form-model-item>
+          </a-col>
+          <a-col span="4">
+            <a-button type="primary" @click="handleSearch" :loading="searchLoading"><a-icon type="search" />查询</a-button>
+          </a-col>
+        </a-row>
+      </a-form>
+
+      <a-table
+        :columns="columns"
+        :data-source="list"
+        :loading="tableLoading"
+        rowKey="deliverId"
+        :pagination="false"
+        size="middle"
+        :scroll="{ x: 1300 }"
+      >
+        <template slot="action" slot-scope="text, record">
+          <div class="action-con">
+            <a-popconfirm
+              title="确认要删除吗?"
+              ok-text="确认"
+              cancel-text="取消"
+              @confirm="handleDelete(record)"
+            >
+              <a class="danger-color"><a-icon class="m-r-4" type="delete" />删除</a>
+            </a-popconfirm>
+          </div>
+        </template>
+      </a-table>
+
+      <div class="pagination-con">
+        <a-pagination
+          show-size-changer
+          show-less-items
+          :current="queryForm.pageNum"
+          :page-size.sync="queryForm.pageSize"
+          :total="paginationTotal"
+          @showSizeChange="onShowSizeChange"
+          @change="changePagination" />
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+import { mixinTableList } from '@/utils/mixin/index'
+
+const columns = [
+  {
+    title: '司机姓名',
+    key: 'driverName',
+    dataIndex: 'driverName',
+    width: 200
+  },
+  {
+    title: '是否加入黑名单',
+    key: 'status',
+    dataIndex: 'status',
+    scopedSlots: { customRender: 'status' },
+    width: 200
+  },
+  {
+    title: '备注',
+    key: 'remark',
+    dataIndex: 'remark',
+    width: 200
+  },
+  {
+    title: '创建时间',
+    key: 'createTime',
+    dataIndex: 'createTime',
+    width: 200
+  },
+  {
+    title: '创建人',
+    key: 'createBy',
+    dataIndex: 'createBy',
+    width: 200
+  },
+  {
+    title: '操作',
+    key: 'action',
+    width: 200,
+    scopedSlots: { customRender: 'action'
+    }
+  }
+]
+
+const queryFormAttr = () => {
+  return {
+    driverName: ''
+  }
+}
+
+export default {
+  name: 'VrDriverDeliverTable',
+  mixins: [mixinTableList],
+  components: {
+  },
+  props: {
+  },
+  data () {
+    return {
+      queryForm: {
+        pageSize: 20,
+        pageNum: 1,
+        ...queryFormAttr()
+      },
+      columns,
+      list: []
+
+    }
+  },
+  methods: {
+    async handleDelete (record) {
+      // try {
+      //   await this.$store.dispatch('blackDriver/destroy', record.driverId)
+      //   this.$message.success('删除成功！')
+
+      //   this.loadTableList()
+      // } catch (error) {
+      //   console.log(error)
+      //   this.$message.error('删除失败，请联系系统管理员！')
+      // }
+    },
+    async loadTableList () {
+      try {
+        this.tableLoading = true
+
+        const { data: { rows, total } } = await this.$store.dispatch('blackDriver/getList', this.queryForm)
+        this.list = rows
+        this.paginationTotal = total
+      } catch (error) {
+        this.$message.error(error.message)
+      } finally {
+        this.tableLoading = false
+      }
+    },
+    async loadData () {
+      this.loadTableList()
+    }
+  },
+  mounted () {
+    this.loadData()
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
---
+++ wms-ui/src/views/vehicleReservation/supplier/ReserveDetailsTable.vue
@@ -0,0 +1,139 @@
+<template>
+  <div class="wrapper">
+    <!-- table -->
+    <div class="table-content">
+      <a-table
+        :columns="columns"
+        :data-source="list"
+        :loading="tableLoading"
+        rowKey="purchaseId"
+        :pagination="false"
+        size="small"
+      >
+      </a-table>
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+import { mixinTableList } from '@/utils/mixin/index'
+
+const columns = [
+  {
+    title: '订单PO号',
+    key: 'poCode',
+    dataIndex: 'poCode',
+    width: 120
+  },
+  {
+    title: '订单行号',
+    key: 'poItem',
+    dataIndex: 'poItem',
+    width: 100
+  },
+  {
+    title: '供应商名称',
+    key: 'supplierName',
+    dataIndex: 'supplierName',
+    width: 180
+  },
+  {
+    title: '料号',
+    key: 'materialCode',
+    dataIndex: 'materialCode',
+    width: 150
+  },
+  {
+    title: '物料名称',
+    key: 'materialName',
+    dataIndex: 'materialName',
+    width: 200
+  },
+  {
+    title: '需求数量',
+    key: 'surplusQuantity',
+    dataIndex: 'surplusQuantity',
+    width: 130
+  },
+  {
+    title: '送货数量',
+    key: 'arriveQuantity',
+    dataIndex: 'arriveQuantity',
+    width: 130
+  },
+  {
+    title: '单位',
+    key: 'unit',
+    dataIndex: 'unit',
+    width: 100
+  }
+]
+
+export default {
+  name: 'VrSupplierReserveDetails',
+  mixins: [mixinTableList],
+  components: {
+  },
+  props: {
+    isVisible: {
+      type: Boolean,
+      default () {
+        return false
+      }
+    },
+    reserveNo: {
+      type: String,
+      default () {
+        return ''
+      }
+    }
+  },
+  data () {
+    return {
+      columns,
+      list: []
+    }
+  },
+  methods: {
+    /** 查询供应商预约详情列表 */
+    async loadTableList () {
+      if (this.reserveNo === '' || this.reserveNo === null) {
+        this.list = []
+        return
+      }
+      try {
+        this.tableLoading = true
+
+        const { data } = await this.$store.dispatch('supplierReserve/getDetailsList', this.reserveNo)
+        this.list = data
+      } catch (error) {
+        this.$message.error(error.message)
+      } finally {
+        console.info(this.list)
+        this.tableLoading = false
+      }
+    },
+    async loadData () {
+      this.loadTableList()
+    }
+  },
+  mounted () {
+    this.loadData()
+  },
+  watch: {
+    isVisible (val) {
+      if (val) {
+        this.loadData()
+      }
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.not-danger-color{
+  color: #aaa
+}
+</style>
