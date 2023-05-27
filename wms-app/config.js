e6d78fb94eb3aa2405a69d2e4586972be94a63ce
xuhao <xuhao2021@gusulab.ac.cn>
2023-05-27 10:39:23 +0800

    bug fix

--- wms-app/config.js
+++ wms-app/config.js
@@ -1,6 +1,6 @@
 const baseApi = {
-	development: "http://121.37.188.213:8080", // "http://192.168.85.161:8080",//本地开发环境地址
-	production: "http://121.37.188.213:8080" //正式环境地址
+	development: "http://192.168.10.199:8080", // "http://192.168.85.161:8080",//本地开发环境地址
+	production: "http://192.168.10.199:8080" //正式环境地址
 }
 
 const NODE_ENV = process.env.NODE_ENV
