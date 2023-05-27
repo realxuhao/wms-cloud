4328bdb9ebf3699eee5009e667f64c65cc58ce21
xuhao <xuhao2021@gusulab.ac.cn>
2023-05-27 13:48:20 +0800

    bug fix

--- wms-ui-cloud/src/utils/screenLog.js
+++ wms-ui-cloud/src/utils/screenLog.js
@@ -2,6 +2,6 @@
 export const printANSI = () => {
   // console.clear()
   // ASCII - ANSI Shadow
-  let text = `纽迪西塔智能仓储平台`
+  let text = `纽迪希亚智能仓储平台`
   console.log(`%c${text}`, 'color: #fc4d50')
 }
