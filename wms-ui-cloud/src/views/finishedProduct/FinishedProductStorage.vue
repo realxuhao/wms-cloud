194c4f2e3f7642d548757e10097ff9416d04aa13
APAC\TJO3SZH <jiadong.tao@cn.bosch.com>
2024-09-19 08:58:38 +0800

    导出模板名称修改

--- wms-ui-cloud/src/views/finishedProduct/FinishedProductStorage.vue
+++ wms-ui-cloud/src/views/finishedProduct/FinishedProductStorage.vue
@@ -306,7 +306,7 @@ export default {
     async handleDownloadTemplate () {
       try {
         // todo 暂时不知道这个文件名
-        this.$store.dispatch('file/downloadByFilename', '入库.xlsx')
+        this.$store.dispatch('file/downloadByFilename', '成品收货.xlsx')
       } catch (error) {
         this.$message.error(error.message)
       }
